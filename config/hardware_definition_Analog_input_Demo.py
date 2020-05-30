c969dda86c9e387b1ebb11a7babff86955a730a2
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-05-30 22:59:52 +0200

    Delete demo config files

--- config/hardware_definition_Analog_input_Demo.py
+++
@@ -1,7 +0,0 @@
-# Demo device setup
-
-from devices import *
-
-# Instantiate Devices.
-# class Analog_input(pin, name, sampling_rate, threshold=None, rising_event=None, falling_event=None)
-
--- config/hardware_definition_Digital_input_Demo.py
+++
@@ -1,8 +0,0 @@
-# Demo device setup
-
-from devices import *
-
-# Instantiate Devices.
-button = Digital_input(pin='X1', rising_event='button_press', falling_event='button_release')
-LED    = Digital_output(pin='X2')
-
